450bc7be6596831b7061e03f242a619343d8ed6e
Twesty <xomu4557@gmail.com>
2020-09-28 16:11:07 +0300

    Product page

--- app/js/common.js
+++ app/js/common.js
@@ -69,4 +69,99 @@ $(function() {
         })
     }
 
+    // Product gallery
+
+    {
+        let thumbnails = $('.gallery__thumbnails');
+        let thumbnail = $('.gallery__thumbnail');
+        let currentImage = $('.gallery__selected');
+
+        let nextArrow = $('.gallery__arrow--next');
+        let prevArrow = $('.gallery__arrow--prev');
+
+        thumbnails.on('init', function(event, slick){
+            $.each(thumbnail, function () {
+                $( this ).on('click', function () {
+                    thumbnail.removeClass('gallery__thumbnail--active');
+                    $( this ).addClass('gallery__thumbnail--active');
+                    currentImage.html($( this ).html());
+                    currentImage.fadeIn("slow");
+                });
+            });
+        });
+
+        thumbnails.slick({
+            slidesToShow: 4,
+            slidesToScroll: 1,
+            vertical: true,
+            arrows: true,
+            prevArrow: prevArrow,
+            nextArrow: nextArrow,
+            swipeToSlide: true,
+            infinite: false,
+            speed: 200
+        });
+
+    }
+
+    // Together slider
+
+    {
+        let items = $('.together__items');
+        let nextArrow = $('.together__arrow--next');
+        let prevArrow = $('.together__arrow--prev');
+
+        items.slick({
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            arrows: true,
+            nextArrow: nextArrow,
+            prevArrow: prevArrow
+        })
+    }
+
+    // Image popup
+
+    {
+        let image = $('.image-popup');
+        let overlay = $('.overlay, .modal-image');
+        let modalImage = $('.modal-image__wrap');
+        let closeButton = $('.modal-image__close');
+
+        let triggersToClose = [overlay, closeButton];
+
+        $.each(image, function () {
+            $( this ).click(function () {
+                overlay.fadeIn(200);
+                modalImage.html($( this ).html());
+                modalImage.fadeIn(200);
+            });
+        });
+
+        $.each(triggersToClose, function () {
+            $( this ).on('click', function () {
+                overlay.fadeOut(200);
+                modalImage.fadeOut(200);
+            })
+        })
+    }
+
+    // FAQ dropdown
+
+    {
+        let items = $('.faq__item');
+
+        $.each(items, function () {
+            let trigger = $( this ).find('.faq__question');
+            let content = $( this ).find('.faq__response');
+
+            trigger.on('click', function () {
+                $( this ).find('.faq__expand').toggleClass('active');
+                content.slideToggle(200);
+            })
+        });
+
+        $('.faq__question').eq(0).trigger('click');
+    }
+
 });
