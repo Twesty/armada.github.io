b18b35c14a50e1ea3b4e672a93cf5dc7c1d5080d
Twesty <xomu4557@gmail.com>
2020-10-22 17:21:46 +0300

    Header phones dropdown

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -2723,6 +2723,27 @@ $( document ).ready(function () {
 
     }
 
+    // Dropdown
+    {
+        let button = $('.dropdown');
+
+        $.each(button, function(){
+            let dropdown = $('.' + $( this ).attr('data-dropdown'));
+
+            $( this ).on('click', function () {
+                dropdown.slideToggle(200);
+            });
+
+            $( document ).mouseup(function(e)
+            {
+                if (!dropdown.is(e.target) && dropdown.has(e.target).length === 0)
+                {
+                    dropdown.slideUp(200);
+                }
+            });
+        });
+    }
+
     // Header media position fixed
 
     {
