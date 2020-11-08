6f22e5a92fdaef47d552c9c954297b38ac3ca5db
Twesty <xomu4557@gmail.com>
2020-11-08 23:05:07 +0200

    Page register validation

--- app/js/dest/page-user-area/user-area.js
+++ app/js/dest/page-user-area/user-area.js
@@ -16,6 +16,25 @@ $( document ).ready(function () {
         });
     }
 
+    // Register validation
+
+    {
+        (function() {
+            window.addEventListener('load', function() {
+                let forms = document.getElementsByClassName('needs-validation');
+                let validation = Array.prototype.filter.call(forms, function(form) {
+                    form.addEventListener('submit', function(event) {
+                        if (form.checkValidity() === false) {
+                            event.preventDefault();
+                            event.stopPropagation();
+                        }
+                        form.classList.add('was-validated');
+                    }, false);
+                });
+            }, false);
+        })();
+    }
+
     // User edit input
 
     {
