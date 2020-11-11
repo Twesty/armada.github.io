109e00a453ff259e7242f6633c9ccebb2437c3ad
Twesty <xomu4557@gmail.com>
2020-11-11 13:55:50 +0200

    Vendor profile page

--- app/js/dest/page-vendor-area/vendor-area.js
+++ app/js/dest/page-vendor-area/vendor-area.js
@@ -22,6 +22,35 @@ $( document ).ready(function () {
         })
     }
 
+    // User edit input
+
+    {
+        let userBlock = $('.user-block');
+
+        $.each(userBlock, function () {
+            let editButton = $( this ).find('.user-block__edit');
+            let inputs = $( this ).find('.user-block-info__value > *:not(span)');
+            let currentValue = $( this ).find('.user-block-info__value > span');
+
+            editButton.on('click', function (e) {
+                e.preventDefault();
+                let form = $( this ).parents('.user-block__wrap');
+
+                if( $( this ).hasClass('active') ) {
+                    form.submit();
+                } else {
+                    $( this ).addClass('active');
+                    editButton.find('span').text('Сохранить');
+                    currentValue.fadeOut(200);
+
+                    setTimeout(function () {
+                        inputs.fadeIn(200);
+                    }, 200);
+                }
+            })
+        })
+    }
+
     // Delete product
 
     {
